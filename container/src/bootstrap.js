5d2d276edf8dbf30e8d8416daeb73814aa37a0bb
Rajat <rajat.sudagade@gmail.com>
2020-11-22 00:52:03 -0600

    final commit

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,2 +1,8 @@
-import 'products/ProductsIndex'
-import 'cart/CartShow';
+import {mount as productMount} from 'products/ProductsIndex'
+import {mount as cartMount} from 'cart/CartShow';
+
+console.log("Container!");
+
+productMount(document.querySelector('#my-products'))
+
+cartMount(document.querySelector('#my-cart'));
